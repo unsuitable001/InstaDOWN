5f7cd1a654a390e6c86ca6a7ba020c5b5578728d
Soumyadip Mondal <mandal.soumyadip9714@gmail.com>
2019-05-19 16:55:51 +0530

    Various fixes.... towards v1.0
    added metadata parser - for a future update (video download)

--- main.js
+++ main.js
@@ -1,4 +1,26 @@
-document.body.onload = function (){
+var isPost=0;
+document.body.onload = function(){
+    isPost = window.location.href.indexOf("instagram.com/p/");
+    if (isPost != -1){
+        downloader();
+    }
+    else{
+        document.body.onclick = function(){
+            setTimeout(() => {
+                isPost = window.location.href.indexOf("instagram.com/p/");
+                console.log(isPost);
+                if (isPost != -1){
+                    if(document.getElementById("iddbtn") == null){
+                        downloader();
+                    }
+                }
+            }, 500);
+        };
+    }
+};
+
+
+function downloader(){
     let btn = window.document.getElementsByClassName('_5e4p')[0];
     let target = window.document.getElementsByClassName('QvAa1');
     let dlObj;
@@ -10,7 +32,8 @@ document.body.onload = function (){
         // Video Downloader goes here.... Work In Progress
     }
     let _nbttn =  btn.cloneNode(true);
-    _nbttn.innerHTML = `<span onclick='window.open("` + dlObj + `");' download>` + _nbttn.innerHTML + "</span>";
+    _nbttn.innerHTML = `<span id="iddbtn" onclick='window.open("` + dlObj + `");' download>` + _nbttn.innerHTML + "</span>";
     _nbttn.setAttribute("style", "transform: rotate(180deg);");
     btn.after(_nbttn);
 }
+
